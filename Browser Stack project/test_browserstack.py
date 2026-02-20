9a41971ffb9b90e5f54621947e9a9f165b178aac
mattekkadannibin-tech <mattekkadannibin@gmail.com>
2026-02-20 10:54:35 +0530

    test_browserstack.py

--- Browser Stack project/test_browserstack.py
+++ Browser Stack project/test_browserstack.py
@@ -3,10 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 
 def test_elpais():
-
     options = webdriver.ChromeOptions()
-
-    # BrowserStack capabilities
     options.set_capability("browserName", "Chrome")
     options.set_capability("browserVersion", "latest")
 
@@ -14,8 +11,13 @@ def test_elpais():
         "os": "Windows",
         "osVersion": "10",
         "sessionName": "ElPais Test",
-        "userName": "Mattekkadan Nibin",   # your username
-        "accessKey": "mattekkadam@123"     # your access key
+        "projectName": "BrowserStack Project",
+        "buildName": "ElPais Build",
+        "userName": "mattekkadannibin_PeQRWC",
+        "accessKey": "auErsxbwsYiXK8yb6rFf",
+        "debug": True,
+        "networkLogs": True,
+        "consoleLogs": "info"
     })
 
     driver = webdriver.Remote(
@@ -26,6 +28,9 @@ def test_elpais():
     driver.get("https://elpais.com/opinion/")
     time.sleep(5)
 
-    print("Title:", driver.title)
+    print("Page Title:", driver.title)
+
+    session_id = driver.session_id
+    print(f"BrowserStack session URL: https://automate.browserstack.com/sessions/{session_id}.json")
 
     driver.quit()
